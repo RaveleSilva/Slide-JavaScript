e6aab3540793315242310920b447c2211889e66f
Ravele Silva <ravele.cs@gmail.com>
2023-09-13 19:39:21 -0300

    Responsividade do slide

--- JavaScript/slide.js
+++ JavaScript/slide.js
@@ -1,9 +1,12 @@
+import debounce from './debounce.js'
+
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
     this.distance = { finalPosition: 0, startX: 0, movement: 0 };
     this.index = { previous: undefined, active: 0, next: undefined };
+    this.activeClass = 'active';
   }
 
   transition(active) {
@@ -69,6 +72,18 @@ export default class Slide {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
+    this.onResize = debounce(this.onResize.bind(this), 200);
+  }
+
+  onResize() {
+    setTimeout(() => {
+      this.slidesConfig();
+      this.changeSlide(this.index.active);
+    }, 1000);
+  }
+
+  addResizeEvent() {
+    window.addEventListener('resize', this.onResize)
   }
 
   slidePosition(slide) {
@@ -97,6 +112,12 @@ export default class Slide {
     this.moveSlide(activeSlide.position);
     this.slidesIndexNav(index);
     this.distance.finalPosition = activeSlide.position;
+    this.changeActiveClass();
+  }
+
+  changeActiveClass() {
+    this.slideArray.forEach(item => item.element.classList.remove(this.activeClass))
+    this.slideArray[this.index.active].element.classList.add(this.activeClass)
   }
 
   activePrevSlide() {
@@ -112,6 +133,7 @@ export default class Slide {
     this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
+    this.addResizeEvent();
     return this;
   }
 }
